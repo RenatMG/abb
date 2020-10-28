155ce0e2369702f1261faeeacf87504fb985d038
Renat <r.detta@yandex.ru>
2020-10-28 17:09:50 +0400

    add margin

--- src/features/PokeList/components/PokeCard.tsx
+++ src/features/PokeList/components/PokeCard.tsx
@@ -12,7 +12,7 @@ interface IPokeCard {
 const PokeCard: React.FC<IPokeCard> = ({pokemon}) => {
     const {id, name, img: src} = pokemon;
     return (
-        <div className='poke-card d-flex flex-wrap col-6 col-md-4 col-lg-3'>
+        <div className='poke-card d-flex flex-wrap col-6 col-md-4 col-lg-3 my-1'>
             <Link to={'/pokemons/' + (name || id)} className='poke-card__container'>
                     <PokeImage src={src} name={name}/>
                     <div className='poke-card__title'>{name}</div>
